84839cb5bff266aed074fe6b484d4b14cc068147
Administrator <Administrator@WIN-D4TPEVLHLJA>
2013-06-16 19:41:58 +0800

    zxx

--- drive-android/src/main/java/com/goodow/drive/android/HelloAndroidActivity.java
+++ drive-android/src/main/java/com/goodow/drive/android/HelloAndroidActivity.java
@@ -1,29 +1,81 @@
 package com.goodow.drive.android;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpRequestFactory;
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.JsonObjectParser;
+import com.google.api.client.json.jackson2.JacksonFactory;
+
 public class HelloAndroidActivity extends Activity {
+	static final HttpTransport HTTP_TRANSPORT = AndroidHttp
+			.newCompatibleTransport();
+	static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
-  /**
-   * Called when the activity is first created.
-   * 
-   * @param savedInstanceState If the activity is being re-initialized after previously being shut
-   *          down then this Bundle contains the data it most recently supplied in
-   *          onSaveInstanceState(Bundle). <b>Note: Otherwise it is null.</b>
-   */
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+	/**
+	 * Called when the activity is first created.
+	 * 
+	 * @param savedInstanceState
+	 *            If the activity is being re-initialized after previously being
+	 *            shut down then this Bundle contains the data it most recently
+	 *            supplied in onSaveInstanceState(Bundle). <b>Note: Otherwise it
+	 *            is null.</b>
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
-  }
+		HttpRequestFactory requestFactory = HTTP_TRANSPORT
+				.createRequestFactory(new HttpRequestInitializer() {
+					@Override
+					public void initialize(HttpRequest request) {
+						request.setParser(new JsonObjectParser(JSON_FACTORY));
+					}
+				});
+
+		GenericUrl url = new GenericUrl("http://imgsrc.baidu.com/forum/crop%3D144%2C0%2C278%2C236%3Bwh%3D200%2C170%3B/sign=16617bd69358d109d0acf3f2ec69f88b/5d2048fbfbedab64c87a5568f636afc379311e39.jpg");
+
+		HttpRequest request;
+		try {
+			request = requestFactory.buildGetRequest(url);
+			HttpResponse response = request.execute();
+			InputStream is = response.getContent();
+			ByteArrayOutputStream bao = new ByteArrayOutputStream();
+			byte[] buffer = new byte[1024];
+			int len = 0;
+			while((len = is.read(buffer))!=-1){
+				bao.write(buffer,0,len);
+			}
+			byte[] dates = bao.toByteArray();
+			
+			FileOutputStream fos = new FileOutputStream(new File("test.jpg"));
+			fos.write(dates);
+			fos.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
 
 }
