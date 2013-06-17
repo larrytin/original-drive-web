9537132b2ababb9fcbeebf8b100a28eb510a53a6
fengjian <fengjian@goodow.com>
2013-06-17 17:05:13 +0800

    add test Tree

---
+++ drive-web/src/main/webapp/good/drive/nav/folders/view.js
@@ -0,0 +1,34 @@
+'use strict';
+goog.provide('good.drive.nav.folders.view');
+
+goog.require('goog.dom');
+goog.require('goog.ui.tree.TreeControl');
+
+var $ = goog.dom.getElement;
+
+function makeTree() {
+	var treeConfig = goog.ui.tree.TreeControl.defaultConfig;
+	treeConfig['cleardotPath'] = '../../images/tree/cleardot.gif';
+	var tree = new goog.ui.tree.TreeControl('我的云端硬盘', treeConfig);
+
+	createTreeFromTestData(tree, testData);
+
+	tree.render($('navfoldersview'));
+	tree.setIsUserCollapsible(true);
+	tree.setShowExpandIcons(false);
+	tree.setExpanded(true);
+}
+
+function createTreeFromTestData(node, data) {
+	for ( var i = 0; i < data.length; i++) {
+		var nodeData = data[i];
+		var childNode = node.getTree().createNode('');
+		node.add(childNode);
+		childNode.setHtml(nodeData[0]);
+		if (nodeData[1]) {
+			createTreeFromTestData(childNode, nodeData[1]);
+		}
+	}
+}
+
+makeTree();
