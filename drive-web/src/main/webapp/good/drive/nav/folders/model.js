ee78bf69345a4316d787cd70992733b287f6fabc
fengjian <fengjian@goodow.com>
2013-06-24 17:18:28 +0800

    change

--- drive-web/src/main/webapp/good/drive/nav/folders/model.js
+++ drive-web/src/main/webapp/good/drive/nav/folders/model.js
@@ -4,31 +4,117 @@ goog.provide('good.drive.nav.folders.Model');
 goog.require('good.drive.nav.folders');
 
 good.drive.nav.folders.Model = function(view) {
-  this.view = view;
+	this.view = view;
+
+	window.modelbak = this;
+	var that = this;
+	good.realtime.authorize('fakeUserId', 'fakeToken');
+	good.realtime.setChannel('http://192.168.1.15:8888');
+	var onInit = function(mod) {
+		that.init(mod);
+	};
+
+	var onLoad = function(doc) {
+		that.doc = doc;
+		that.mod = that.doc.getModel();
+		that.root = that.mod.getRoot();
+
+		// connectUi();
+		that.connect(doc);
+	};
+	good.realtime.load('@tmp/a9', onLoad, onInit, null);
 };
 
 good.drive.nav.folders.Model.prototype.connect = function(doc) {
-  this.doc = doc;
-  this.mod = doc.getModel();
-  this.root = mod.getRoot();
-  var folders = this.root.get('folders');
-  this.addEventListener({
-    dom: this.view.tree.rootDom}, folders);
+	var folders = this.root.get('folders');
+	var that = this;
+	this.addEventListener({
+		dom : that.view.tree
+	}, folders);
 };
 
-good.drive.nav.folders.Model.prototype.addEventListener = function(parentDom, list) {
-  var that = this;
-  var childrenDoms = [];
-  list.addValuesAddedListener(function(evt) {
-    var idx = evt.getIndex();
-    var vals = evt.getValues();
-    for (var i in vals) {
-      childrenDoms[i].dom = that.view.insertNode(parentDom.dom, idx + i, vals[i]);
-    }
-  });
-  for (var i = 0, len = list.length(); i < len; i++) {
-    var children = list.get(i).get(1);
-    childrenDoms[i] = {};
-    this.addEventListener(childrenDoms[i], children);
-  }
+good.drive.nav.folders.Model.prototype.addEventListener = function(
+		parentHolder, list) {
+	var that = this;
+	var childrenDoms = [];
+	list.addValuesAddedListener(function(evt) {
+		var idx = evt.getIndex();
+		var vals = evt.getValues();
+		var parentDom = parentHolder.dom;
+		var isAdd = false;
+		if(parentDom.getChildCount() != 0) {
+			isAdd = true;
+		}
+		for ( var i in vals) {
+			var val = vals[i];
+			var arrayIdx = i;
+			if(isAdd) {
+				var arrayIdx = idx + parseInt(i);
+				childrenDoms[arrayIdx] = {};
+				if(goog.isObject(val)) {
+					childrenDoms[arrayIdx].dom = that.view.insertFolder(parentDom, idx
+							+ i, [val]);
+//					that.addEventListener(childrenDoms[arrayIdx], val.get(1));
+					continue;
+				}
+			}
+			childrenDoms[arrayIdx].dom = that.view.insertNode(parentDom, idx
+					+ i, val);
+		}
+		isAdd = false;
+	});
+	for ( var i = 0, len = list.length(); i < len; i++) {
+		var value = list.get(i);
+		childrenDoms[i] = {};
+		if (goog.isString(value)) {
+			continue;
+		}
+		var children = value.get(1);
+		this.addEventListener(childrenDoms[i], children);
+	}
 };
+
+good.drive.nav.folders.Model.prototype.init = function(mod) {
+	var name = [ "我的课件", "我的音乐", "我的视频", "我的科学" ];
+	var testdata = mod.getRoot();
+	var rootlist;
+	var leaflist;
+
+	var folders = mod.createList();
+	
+	for(var i in name) {
+		rootlist = mod.createList();
+		folders.push(rootlist);
+	}
+
+	for ( var i in name) {
+		leaflist = mod.createList();
+		folders.get(i).push(name[i]);
+		folders.get(i).push(leaflist);
+		leaflist.push(name[i] + "a");
+		leaflist.push(name[i] + "b");
+		leaflist.push(name[i] + "c");
+		leaflist.push(name[i] + "d");
+
+//		rootlist = mod.createList();
+//		rootlist.push(name[i] + "一年级");
+//		leaflist = mod.createList();
+//		leaflist.push(name[i] + "a");
+//		leaflist.push(name[i] + "b");
+//		leaflist.push(name[i] + "c");
+//		leaflist.push(name[i] + "d");
+//		folders.get(i).get(1).push(rootlist);
+	}
+	testdata.set("folders", folders);
+
+};
+
+good.drive.nav.folders.Model.prototype.addFolder = function(list, idx, data) {
+	var str = list.get(idx);
+	var root = this.mod.createList();
+	var leaf = this.mod.createList();
+	root.push(str);
+	root.push(leaf);
+	leaf.push(data);
+	list.replaceRange(idx, root);
+}
--- drive-web/src/main/webapp/good/drive/nav/folders/view.js
+++ drive-web/src/main/webapp/good/drive/nav/folders/view.js
@@ -1,14 +1,14 @@
 'use strict';
 goog.provide('good.drive.nav.folders');
 
-goog.require('good.drive.nav.folders.Model');
 goog.require('goog.dom');
 goog.require('goog.ui.tree.TreeControl');
+goog.require('good.drive.nav.folders.Model');
 
-good.drive.nav.folders.view.Tree = function() {
+good.drive.nav.folders.Tree = function() {
 	var treeConfig = goog.ui.tree.TreeControl.defaultConfig;
 	treeConfig['cleardotPath'] = '../../images/tree/cleardot.gif';
-	var tree_ = new goog.ui.tree.TreeControl('我的云端硬盘', treeConfig);
+	var tree_ = new goog.ui.tree.TreeControl('我的资料库', treeConfig);
 	tree_.render(goog.dom.getElement('navfolderslist'));
 	tree_.setIsUserCollapsible(true);
 	tree_.setShowExpandIcons(true);
@@ -16,32 +16,96 @@ good.drive.nav.folders.view.Tree = function() {
 
 	this.tree = tree_;
 
-	var that = this;
-	good.realtime.authorize('fakeUserId', 'fakeToken');
-	good.realtime.setChannel('http://192.168.1.15:8888');
-	var onInit = function(mod) {
-		importData(mod);
-	};
+	var model_ = new good.drive.nav.folders.Model(this);
+	this.model = model_
+	this.model.connect();
 
-	var onLoad = function(doc) {
-		that.doc = doc;
-		that.mod = that.doc.getModel();
-		that.root = that.mod.getRoot();
-
-		that.renderTree(doc);
-		// connectUi();
-		that.connectRealtime(doc);
-	};
-	good.realtime.load('@tmp/myFolders1', onLoad, onInit, null);
+	/*
+	 * var that = this; good.realtime.authorize('fakeUserId', 'fakeToken');
+	 * good.realtime.setChannel('http://192.168.1.15:8888'); var onInit =
+	 * function(mod) { importData(mod); };
+	 * 
+	 * var onLoad = function(doc) { that.doc = doc; that.mod =
+	 * that.doc.getModel(); that.root = that.mod.getRoot();
+	 * 
+	 * that.renderTree(doc); // connectUi(); that.connectRealtime(doc); };
+	 * good.realtime.load('@tmp/myFolders1', onLoad, onInit, null);
+	 */
 };
 
-good.drive.nav.folders.view.Tree.prototype.renderTree = function(doc) {
-	this.render(this.tree, this.root.get('folders'));
+/*
+ * good.drive.nav.folders.Tree.prototype.renderTree = function(doc) {
+ * this.render(this.tree, this.root.get('folders')); };
+ */
+
+/*good.drive.nav.folders.Tree.prototype.render = function(parent, data, idx) {
+ data = goog.isArray(data) ? data : data.asArray();
+ for ( var i = 0; i < data.length; i++) {
+ var childNode = parent.getTree().createNode('');
+ if (idx == undefined) {
+ parent.add(childNode);
+ } else {
+ if (idx < parent.getChildCount() - 1) {
+ parent.removeChildAt(idx + i);
+ }
+ parent.addChildAt(childNode, idx + i);
+ }
+ var value = data[i];
+ if (goog.isString(value)) {
+ childNode.setHtml(value);
+ continue;
+ }
+ childNode.setHtml(value.get(0));
+ if (value.get(1)) {
+ this.render(childNode, value.get(1));
+ }
+ }
+ };*/
+
+/*
+ * good.drive.nav.folders.Tree.prototype.connectRealtime = function(doc) { var
+ * list = this.root.get('folders'); this.listenerBind(this.tree, list); };
+ */
+
+/*
+ * good.drive.nav.folders.Tree.prototype.listenerBind = function(parent, list,
+ * idx) { this.bind(parent, list); if (list.length() > 1) { for ( var i = 0; i <
+ * list.length(); i++) { var children; if(idx == undefined) { children =
+ * parent.getChildAt(i); } else { children = parent.getChildAt(idx);
+ * if(goog.isObject(list.get(i))) { this.listenerBind(children, list.get(i)); }
+ * continue; } var value = list.get(i); if(goog.isString(value)) { continue; }
+ * if(goog.isObject(value) && value.length() == 2) { this.bind(parent, value); }
+ * this.listenerBind(children, value.get(1)); } } }
+ */
+
+/*
+ * good.drive.nav.folders.Tree.prototype.bind = function(parent, list) { var
+ * that = this; var init = true; var event = function(evt) { if(init) { init =
+ * false; return; } var parent_ = parent; var children = evt.getValues(); var
+ * idx = evt.getIndex(); that.insertNodes(parent, idx, children) for(var c in
+ * children) { if(goog.isObject(children[c])) { that.listenerBind(parent,
+ * children[c], idx); } } } list.addValuesAddedListener(event); }
+ */
+
+good.drive.nav.folders.Tree.prototype.insertNode = function(parent, idx, value) {
+	console.log(parent.getHtml() + "-" + idx + "-" + value);
+	// this.render(parent, children, idx);
+	var title = goog.isString(value) ? value : value.get(0);
+	var childNode = parent.getTree().createNode('');
+	// if (idx < parent.getChildCount() - 1) {
+	// parent.removeChildAt(idx);
+	// parent.addChildAt(childNode, idx);
+	// } else {
+	parent.add(childNode);
+	// }
+	childNode.setHtml(title);
+	return childNode;
 };
 
-good.drive.nav.folders.view.Tree.prototype.render = function(parent, data, idx) {
-	data = goog.isArray(data) ? data : data.asArray();
-	for (var i = 0; i < data.length; i++) {
+good.drive.nav.folders.Tree.prototype.insertFolder = function(parent, idx,
+		value) {
+	value = goog.isArray(value) ? value : value.asArray();
+	for ( var i = 0; i < value.length; i++) {
 		var childNode = parent.getTree().createNode('');
 		if (idx == undefined) {
 			parent.add(childNode);
@@ -51,126 +115,20 @@ good.drive.nav.folders.view.Tree.prototype.render = function(parent, data, idx) 
 			}
 			parent.addChildAt(childNode, idx + i);
 		}
-		var value = data[i];
+		var value = value[i];
 		if (goog.isString(value)) {
 			childNode.setHtml(value);
 			continue;
 		}
 		childNode.setHtml(value.get(0));
 		if (value.get(1)) {
-			this.render(childNode, value.get(1));
+			this.insertFolder(childNode, value.get(1));
 		}
 	}
-};
+}
 
-good.drive.nav.folders.view.Tree.prototype.connectRealtime = function(doc) {
-	var list = this.root.get('folders');
-	// this.listenerBind(this.tree, list);
-	this.addEventListener(list);
-};
+good.drive.nav.folders.start = function() {
+	var tree_ = new good.drive.nav.folders.Tree();
+}
 
-good.drive.nav.folders.view.Tree.prototype.addEventListener = function(list) {
-  list.addValuesAddedListener(function(evt) {
-
-  });
-  for (var i = 0, len = list.length(); i < len; i++) {
-
-  }
-};
-
-good.drive.nav.folders.view.Tree.prototype.listenerBind = function(parent, list, idx) {
-	this.bind(parent, list);
-	if (list.length() > 1) {
-		for (var i = 0; i < list.length(); i++) {
-			var children;
-			if (idx == undefined) {
-				children = parent.getChildAt(i);
-			} else {
-				children = parent.getChildAt(idx);
-				if (goog.isObject(list.get(i))) {
-					this.listenerBind(children, list.get(i));
-				}
-				continue;
-			}
-			var value = list.get(i);
-			if (goog.isString(value)) {
-				continue;
-			}
-			if (goog.isObject(value) && value.length() == 2) {
-				this.bind(parent, value);
-			}
-			this.listenerBind(children, value.get(1));
-		}
-	}
-};
-
-good.drive.nav.folders.view.Tree.prototype.listenerUnBind = function(parent, list, idx) {
-	this.bind(parent, list);
-	if (list.length() > 1) {
-		for (var i = 0; i < list.length(); i++) {
-			var children;
-			if (idx == undefined) {
-				children = parent.getChildAt(i);
-			} else {
-				children = parent.getChildAt(idx);
-				if (goog.isObject(list.get(i))) {
-					this.listenerBind(children, list.get(i));
-				}
-				continue;
-			}
-			var value = list.get(i);
-			if (goog.isString(value)) {
-				continue;
-			}
-			if (goog.isObject(value) && value.length() == 2) {
-				this.bind(parent, value);
-			}
-			this.listenerBind(children, value.get(1));
-		}
-	}
-};
-
-good.drive.nav.folders.view.Tree.prototype.bind = function(parent, list) {
-	var that = this;
-	var init = true;
-	var event = function(evt) {
-		if (init) {
-			init = false;
-			return;
-		}
-		var parent_ = parent;
-		var children = evt.getValues();
-		var idx = evt.getIndex();
-		that.insertNodes(parent, idx, children);
-		for (var c in children) {
-			if (goog.isObject(children[c])) {
-				that.listenerBind(parent, children[c], idx);
-			}
-		}
-	};
-	list.addValuesAddedListener(event);
-};
-
-good.drive.nav.folders.view.Tree.prototype.insertNodes = function(parent, idx,
-    children) {
-	console.log(parent.getHtml() + '-' + idx + '-' + children);
-	this.render(parent, children, idx);
-};
-
-good.drive.nav.folders.view.Tree.prototype.addFolder = function(list, idx, data) {
-	var str = list.get(idx);
-	var leaf = this.mod.createList();
-	leaf.push(data);
-	var root = this.mod.createList();
-	root.push(str);
-	root.push(leaf);
-	list.replaceRange(idx, root);
-};
-
-good.drive.nav.folders.view.start = function() {
-	var tree = new good.drive.nav.folders.view.Tree();
-	window.tree = tree;
-};
-
-goog.exportSymbol('good.drive.nav.folders.view.start',
-		good.drive.nav.folders.view.start);
+goog.exportSymbol('good.drive.nav.folders.start', good.drive.nav.folders.start);
