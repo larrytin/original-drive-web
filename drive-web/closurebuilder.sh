1e087aabcb0d3de74db736d68f484ebe5ac9f55b
larry <dev@goodow.com>
2013-06-30 15:23:08 +0800

    Enable source maps

--- drive-web/closurebuilder.sh
+++ drive-web/closurebuilder.sh
@@ -1,10 +1,13 @@
-src/main/webapp/good.js/closure-library/closure/bin/build/closurebuilder.py \
-  --root=src/main/webapp/good.js/closure-library/ \
-  --root=src/main/webapp/good.js/good/ \
-  --root=src/main/webapp/good/ \
+cd src/main/webapp
+good.js/closure-library/closure/bin/build/closurebuilder.py \
+  --root=good.js/closure-library/ \
+  --root=good.js/good/ \
+  --root=good/ \
   --namespace="good.drive.init" \
   --output_mode=compiled \
   --compiler_jar=$HOME/.m2/repository/com/goodow/javascript/closure-compiler/v20130603-SNAPSHOT/closure-compiler-v20130603-SNAPSHOT.jar \
   --compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS" \
-  --compiler_flags="--externs=src/main/webapp/good.js/good/realtime/realtime.externs.js" \
-  > src/main/webapp/compiled.js
+  --compiler_flags="--externs=good.js/good/realtime/realtime.externs.js" \
+  --compiler_flags="--create_source_map=drive.js.map" \
+  > drive.js
+echo //@ sourceMappingURL=drive.js.map >> drive.js
