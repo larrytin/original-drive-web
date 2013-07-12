349bef27c2a9aa8a0f851a819a7dc59d9168ca73
norven63 <norven63@163.com>
2013-07-12 18:09:45 +0800

    Remote control.

--- drive-android/src/main/java/com/goodow/drive/android/fragment/DataListFragment.java
+++ drive-android/src/main/java/com/goodow/drive/android/fragment/DataListFragment.java
@@ -15,7 +15,6 @@ import com.goodow.realtime.ValuesAddedEvent;
 import com.goodow.realtime.ValuesRemovedEvent;
 import com.goodow.realtime.ValuesSetEvent;
 import java.util.ArrayList;
-import java.util.List;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -27,188 +26,252 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 public class DataListFragment extends ListFragment {
-  private Button backButton;
+	private Button backButton;
 
-  private List<CollaborativeMap> historyOpenedFolders = new ArrayList<CollaborativeMap>();
-  private CollaborativeMap currentFolder = null;
+	private CollaborativeList historyOpenedFolders;
+	private CollaborativeMap currentFolder = null;
 
-  private CollaborativeAdapter adapter;
+	private CollaborativeAdapter adapter;
 
-  private Document doc;
-  private Model model;
-  private CollaborativeMap root;
-  private CollaborativeList list;
-  private static final String DATA_KEY = "folders";
-  private EventHandler<ValuesAddedEvent> valuesAddedEventHandler;
-  private EventHandler<ValuesRemovedEvent> valuesRemovedEventHandler;
-  private EventHandler<ValuesSetEvent> valuesSetEventHandler;
+	private Document doc;
+	private Model model;
+	private CollaborativeMap root;
 
-  private ArrayList<Integer> canOpen = new ArrayList<Integer>();// 标注哪些是文件夹,可以点击进入
+	private static final String FOLDER_KEY = "folderschild";
+	private static final String FILE_KEY = "filechild";
+	private static final String PATH_KEY = "path";
 
-  public void addCanOpenItem(Integer position) {
-    canOpen.add(position);
-  }
+	private EventHandler<ValuesAddedEvent> pathValuesAddedEventHandler;
+	private EventHandler<ValuesRemovedEvent> pathValuesRemovedEventHandler;
+//	private EventHandler<ValuesSetEvent> pathValuesSetEventHandler;
 
-  public void backFragment() {
-    if (null != currentFolder) {
-      currentFolder = historyOpenedFolders.get(historyOpenedFolders.size() - 1);
-      historyOpenedFolders.remove(historyOpenedFolders.size() - 1);
+	private EventHandler<ValuesAddedEvent> valuesAddedEventHandler;
+	private EventHandler<ValuesRemovedEvent> valuesRemovedEventHandler;
+	private EventHandler<ValuesSetEvent> valuesSetEventHandler;
 
-      if (0 != historyOpenedFolders.size()) {
-        initData();
-      } else {
-        adapter.setFolderList(list);
-        freshListData();
-      }
+	private ArrayList<Integer> canOpen = new ArrayList<Integer>();// 标注哪些文件夹可以点击进入
 
-    } else {
-      Toast.makeText(getActivity(), R.string.backFolderErro, Toast.LENGTH_SHORT).show();
-    }
-  }
+	public void addCanOpenItem(Integer position) {
+		canOpen.add(position);
+	}
 
-  public void connectUi() {
-    list = root.get(DATA_KEY);
+	public void backFragment() {
+		if (1 != historyOpenedFolders.length()) {
+			historyOpenedFolders.remove(historyOpenedFolders.length() - 1);
+		} else {
+			Toast.makeText(getActivity(), R.string.backFolderErro,
+					Toast.LENGTH_SHORT).show();
+		}
+	}
 
-    adapter.setFolderList(list);
+	public void connectUi() {
+		historyOpenedFolders = root.get(PATH_KEY);
+		historyOpenedFolders
+				.addValuesAddedListener(pathValuesAddedEventHandler);
+		historyOpenedFolders
+				.addValuesRemovedListener(pathValuesRemovedEventHandler);
 
-    setListListener(list);
-  }
+		historyOpenedFolders.push(root);
+	}
 
-  public void freshListData() {
-    canOpen.clear();
-    adapter.notifyDataSetChanged();
-  }
+	public void freshListData() {
+		canOpen.clear();
+		adapter.notifyDataSetChanged();
+	}
 
-  public void initData() {
-    if (null != currentFolder) {
-      CollaborativeList folderList = (CollaborativeList) currentFolder.get("folderschild");
-      CollaborativeList fileList = (CollaborativeList) currentFolder.get("filechild");
+	public void initData() {
+		if (null != currentFolder) {
+			CollaborativeList folderList = (CollaborativeList) currentFolder
+					.get(FOLDER_KEY);
+			CollaborativeList fileList = (CollaborativeList) currentFolder
+					.get(FILE_KEY);
 
-      adapter.setFolderList(folderList);
-      adapter.setFileList(fileList);
-      freshListData();
+			adapter.setFolderList(folderList);
+			adapter.setFileList(fileList);
+			freshListData();
 
-      setListListener(folderList);
-      setListListener(fileList);
-    }
-  }
+			if (null != folderList) {
+				setListListener(folderList);
+			}
 
-  @Override
-  public void onActivityCreated(Bundle savedInstanceState) {
-    super.onActivityCreated(savedInstanceState);
+			if (null != fileList) {
+				setListListener(fileList);
+			}
+		}
+	}
 
-    backButton = (Button) getActivity().findViewById(R.id.backButton);
-    backButton.setOnClickListener(new OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        backFragment();
-      }
-    });
-  }
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
 
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    adapter = new CollaborativeAdapter(this, this.getActivity(), null, null);
-    setListAdapter(adapter);
+		backButton = (Button) getActivity().findViewById(R.id.backButton);
+		backButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				backFragment();
+			}
+		});
+	}
 
-    valuesAddedEventHandler = new EventHandler<ValuesAddedEvent>() {
-      @Override
-      public void handleEvent(ValuesAddedEvent event) {
-        freshListData();
-      }
-    };
+	private void initEventHandler() {
+		if (valuesAddedEventHandler == null) {
+			valuesAddedEventHandler = new EventHandler<ValuesAddedEvent>() {
+				@Override
+				public void handleEvent(ValuesAddedEvent event) {
+					freshListData();
+				}
+			};
+		}
 
-    valuesRemovedEventHandler = new EventHandler<ValuesRemovedEvent>() {
-      @Override
-      public void handleEvent(ValuesRemovedEvent event) {
-        freshListData();
-      }
-    };
+		if (valuesRemovedEventHandler == null) {
+			valuesRemovedEventHandler = new EventHandler<ValuesRemovedEvent>() {
+				@Override
+				public void handleEvent(ValuesRemovedEvent event) {
+					freshListData();
+				}
+			};
+		}
 
-    valuesSetEventHandler = new EventHandler<ValuesSetEvent>() {
-      @Override
-      public void handleEvent(ValuesSetEvent event) {
-        freshListData();
-      }
-    };
+		if (valuesSetEventHandler == null) {
+			valuesSetEventHandler = new EventHandler<ValuesSetEvent>() {
+				@Override
+				public void handleEvent(ValuesSetEvent event) {
+					freshListData();
+				}
+			};
+		}
 
-    DocumentLoadedHandler onLoaded = new DocumentLoadedHandler() {
-      @Override
-      public void onLoaded(Document document) {
-        doc = document;
-        model = doc.getModel();
-        root = model.getRoot();
+		if (pathValuesAddedEventHandler == null) {
+			pathValuesAddedEventHandler = new EventHandler<ValuesAddedEvent>() {
+				@Override
+				public void handleEvent(ValuesAddedEvent event) {
+					if (0 != historyOpenedFolders.length()) {
+						currentFolder = historyOpenedFolders
+								.get(historyOpenedFolders.length() - 1);
+						
+						if (null == currentFolder.get(FOLDER_KEY)) {
+							// TODO
+							Toast.makeText(DataListFragment.this.getActivity(),
+									"你打开了一个文件!正在播放...", Toast.LENGTH_SHORT)
+									.show();
+						} else {
+							initData();
+						}
+					}
+				}
+			};
+		}
 
-        connectUi();
+		if (pathValuesRemovedEventHandler == null) {
+			pathValuesRemovedEventHandler = new EventHandler<ValuesRemovedEvent>() {
+				@Override
+				public void handleEvent(ValuesRemovedEvent event) {
+					if (0 != historyOpenedFolders.length()) {
+						currentFolder = historyOpenedFolders
+								.get(historyOpenedFolders.length() - 1);
+						
+						if (null == currentFolder.get(FOLDER_KEY)) {
+							// TODO
+							Toast.makeText(DataListFragment.this.getActivity(),
+									"你打开了一个文件!正在播放...", Toast.LENGTH_SHORT)
+									.show();
+						} else {
+							initData();
+						}
+					}
+				}
+			};
+		}
+	}
 
-      }
-    };
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		adapter = new CollaborativeAdapter(this, this.getActivity(), null, null);
+		setListAdapter(adapter);
 
-    ModelInitializerHandler initializer = new ModelInitializerHandler() {
-      @Override
-      public void onInitializer(Model model_) {
-        model = model_;
-        root = model.getRoot();
+		initEventHandler();
 
-        String[] mapKey = { "label", "filechild", "folderschild" };
-        CollaborativeMap[] values = new CollaborativeMap[3];
+		// 文件Document
+		DocumentLoadedHandler onLoaded = new DocumentLoadedHandler() {
+			@Override
+			public void onLoaded(Document document) {
+				doc = document;
+				model = doc.getModel();
+				root = model.getRoot();
 
-        for (int k = 0; k < values.length; k++) {
-          CollaborativeMap map = model.createMap(null);
-          for (int i = 0; i < mapKey.length; i++) {
-            if ("label".equals(mapKey[i])) {
-              map.set(mapKey[i], "Folder" + k);
-            } else {
-              CollaborativeList subList = model.createList();
+				connectUi();
+			}
+		};
 
-              if ("folderschild".equals(mapKey[i])) {
-                CollaborativeMap subMap = model.createMap(null);
-                subMap.set("label", "SubFolder");
-                subMap.set("filechild", model.createList());
-                subMap.set("folderschild", model.createList());
-                subList.push(subMap);
-              }
+		ModelInitializerHandler initializer = new ModelInitializerHandler() {
+			@Override
+			public void onInitializer(Model model_) {
+				model = model_;
+				root = model.getRoot();
 
-              map.set(mapKey[i], subList);
-            }
-          }
+				String[] mapKey = { "label", "filechild", "folderschild" };
+				CollaborativeMap[] values = new CollaborativeMap[3];
 
-          values[k] = map;
-        }
+				for (int k = 0; k < values.length; k++) {
+					CollaborativeMap map = model.createMap(null);
+					for (int i = 0; i < mapKey.length; i++) {
+						if ("label".equals(mapKey[i])) {
+							map.set(mapKey[i], "Folder" + k);
+						} else {
+							CollaborativeList subList = model.createList();
 
-        CollaborativeList list = model_.createList();
-        list.pushAll((Object[]) values);
+							if ("folderschild".equals(mapKey[i])) {
+								CollaborativeMap subMap = model.createMap(null);
+								subMap.set("label", "SubFolder");
+								subMap.set("filechild", model.createList());
+								subMap.set("folderschild", model.createList());
+								subList.push(subMap);
+							}
 
-        root.set("folders", list);
-      }
-    };
+							map.set(mapKey[i], subList);
+						}
+					}
 
-    String docId = "@tmp/" + GlobalDataCacheForMemorySingleton.getInstance().getUserId() + "/androidTest02";
-    Realtime.load(docId, onLoaded, initializer, null);
-  }
+					values[k] = map;
+				}
 
-  @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.folder_list, container, false);
-  }
+				CollaborativeList list = model_.createList();
+				list.pushAll((Object[]) values);
 
-  @Override
-  public void onListItemClick(ListView l, View v, int position, long id) {
-    if (canOpen.contains(position)) {
-      CollaborativeMap clickItem = (CollaborativeMap) v.getTag();
-      historyOpenedFolders.add(currentFolder);
-      currentFolder = clickItem;
+				root.set("folders", list);
 
-      initData();
-    }
-  }
+				list = model_.createList();
+				root.set("path", list);
+			}
+		};
 
-  private void setListListener(CollaborativeList listenerList) {
-    listenerList.addValuesSetListener(valuesSetEventHandler);
+		String docId = "@tmp/"
+				+ GlobalDataCacheForMemorySingleton.getInstance().getUserId()
+				+ "/androidTest02";
+		Realtime.load(docId, onLoaded, initializer, null);
 
-    listenerList.addValuesRemovedListener(valuesRemovedEventHandler);
+	}
 
-    listenerList.addValuesAddedListener(valuesAddedEventHandler);
-  }
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.folder_list, container, false);
+	}
+
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		if (canOpen.contains(position)) {
+			CollaborativeMap clickItem = (CollaborativeMap) v.getTag();
+			historyOpenedFolders.push(clickItem);
+		}
+	}
+
+	private void setListListener(CollaborativeList listenerList) {
+		listenerList.addValuesSetListener(valuesSetEventHandler);
+
+		listenerList.addValuesRemovedListener(valuesRemovedEventHandler);
+
+		listenerList.addValuesAddedListener(valuesAddedEventHandler);
+	}
 }
