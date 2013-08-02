68c95551cb687477cd30d0b5ff71b6ba6a9fd2bb
fengjian <fengjian@goodow.com>
2013-08-02 15:05:37 +0800

    merge

--- drive-web/src/main/webapp/good/drive/nav/grid/cell.js
+++ drive-web/src/main/webapp/good/drive/nav/grid/cell.js
@@ -20,6 +20,7 @@ good.drive.nav.grid.Cell = function(data, keytype, defaultConfig, opt_domHelper)
   this.keytype = keytype;
   this.isFolder_ = undefined;
   this.selected_ = false;
+  this.bindHandle(data);
 };
 goog.inherits(good.drive.nav.grid.Cell, goog.ui.Component);
 
@@ -68,6 +69,24 @@ good.drive.nav.grid.Cell.prototype.exitDocument = function() {
   this.detachEvents_();
 };
 
+
+good.drive.nav.grid.Cell.prototype.bindHandle = function(data) {
+  var that = this;
+  if (data instanceof good.realtime.CollaborativeMap) {
+    data.addValueChangedListener(function(evt) {
+      var property = evt.getProperty();
+      if (property != 'label') {
+        return;
+      }
+      var newValue = evt.getNewValue();
+      var oldValue = evt.getOldValue();
+      if (oldValue == null) {
+        return;
+      }
+      that.renderCell();
+    });
+  }
+};
 
 /**
  * @return {boolean}
@@ -147,12 +166,17 @@ good.drive.nav.grid.Cell.prototype.handleKeyEvent = function(e) {
  * @param {goog.events.BrowserEvent} e
  */
 good.drive.nav.grid.Cell.prototype.clickHandle = function(e) {
-  if (!(this.data instanceof good.realtime.CollaborativeMap)) {
-    return;
-  }
-  var path = good.drive.nav.folders.Path.getINSTANCE().pathlist;
-  path.push(this.data.getId());
+  this.openCell();
 };
+
+good.drive.nav.grid.Cell.prototype.openCell = function() {
+  if (this.data instanceof good.realtime.CollaborativeMap) {
+    var path = good.drive.nav.folders.Path.getINSTANCE().pathlist;
+    path.push(this.data.getId());
+  } else {
+    
+  }
+}
 
 /** @override */
 good.drive.nav.grid.Cell.prototype.createDom = function() {
@@ -288,5 +312,7 @@ good.drive.nav.grid.Cell.prototype.getLabelClassName = function() {
  * @param {Element} dom
  */
 good.drive.nav.grid.Cell.prototype.setLabel = function(dom) {
+  goog.dom.removeChildren(this.getImageContainerElement());
+  goog.dom.removeChildren(this.getLabelElement());
   goog.dom.appendChild(this.getLabelElement(), dom);
 };
