460ed3cd0c480532613153819197826377e39e7c
xiaozhu2020 <364565139@qq.com>
2013-08-13 10:21:54 +0800

    add user manage

--- drive-web/src/main/webapp/good/drive/person/listperson.js
+++ drive-web/src/main/webapp/good/drive/person/listperson.js
@@ -13,28 +13,99 @@ goog.require('goog.events.EventType');
 good.drive.person.Listperson = function() {
   var personman = goog.dom.getElement('personman');
   this._personman = personman;
+  var grid = good.drive.person.Listperson.SEARCHGRID;
+  if (grid == undefined) {
+    grid = new good.drive.view.table.View(
+        {'select': 'select', 'displayName': '姓名',
+          'name': '用户名'});
+    grid.render(goog.dom.getElement('tableviewmanager'));
+    good.drive.person.Listperson.SEARCHGRID = grid;
+    good.drive.view.baseview.View.visiable(grid);
+  }
+  
+  var view = new good.drive.person.View();
+  var dialog = view.createDailog('编辑人员', function(evt) {
+  switch (evt.key) {
+    case 'cr':
+      that.insertOrUpdate();
+      if (good.drive.person.AddPerson.FLAG) {
+        return false;
+      }
+      break;
+      case 'c':
+        break;
+      default:
+        break;
+    }
+  });
+  this._dialog = dialog;
+  this._view = view;
+};
 
-  goog.events.listen(personman, goog.events.EventType.CLICK, function(e) {
-//    var rpc = new good.net.CrossDomainRpc('GET',
-//        good.config.ACCOUNT,
-//        good.config.VERSION,
-//        'accountinfo',
-//        good.config.SERVERADRESS);
-//    rpc.send(function(json) {
-//      if (json && !json['error']) {
-//        var grid = new good.drive.view.table.View(
-//          {'select': 'select', 'displayName': '姓名',
-//            'name': '用户名'}, json['items']);
-//        goog.array.forEach(json['items'], function(item) {
-//          var cell = grid.createCell(item);
-//          cell.getValue = function(key) {
-//            return this.data[key];
-//          };
-//          grid.add(cell);
-//          cell.renderCell();
-//        });
-//        good.drive.view.baseview.View.visiable(grid);
-//      }
-//    });
+/** Type {good.drive.view.table.View} */
+good.drive.person.Listperson.SEARCHGRID = undefined;
+
+/** Type {string} */
+good.drive.person.Listperson.USERID = undefined;
+
+/**
+ *
+ */
+good.drive.person.Listperson.prototype.searchPerson = function() {
+  var rpc = new good.net.CrossDomainRpc('GET',
+      good.config.ACCOUNT,
+      good.config.VERSION,
+      'accountinfo',
+      good.config.SERVERADRESS);
+  rpc.send(function(json) {
+    if (json && !json['error']) {
+      var grid = good.drive.person.Listperson.SEARCHGRID;
+      goog.array.forEach(json['items'], function(item) {
+        var cell = grid.createCell(item);
+        cell.getValue = function(key) {
+          return this.data[key];
+        };
+        grid.add(cell);
+        cell.renderCell();
+      });
+      good.drive.view.baseview.View.visiable(grid);
+    }
   });
 };
+
+/**
+ * @param {string} userId
+ */
+good.drive.person.Listperson.prototype.deletePerson = function(userId) {
+  var that = this;
+  var rpc = new good.net.CrossDomainRpc('POST',
+      good.config.ACCOUNT,
+      good.config.VERSION,
+      'accountinfo' + userId,
+      good.config.SERVERADRESS);
+  rpc.send(function(json) {
+    if (json && !json['error']) {
+      alert('删除成功！');
+      that.searchPerson();
+    }
+  });
+};
+
+/**
+ * @param {string} userId
+ */
+good.drive.person.Listperson.prototype.editPerson = function(userId) {
+  good.drive.person.Listperson.USERID = userId;
+  var that = this;
+  var rpc = new good.net.CrossDomainRpc('GET',
+      good.config.ACCOUNT,
+      good.config.VERSION,
+      'accountinfo' + personId,
+      good.config.SERVERADRESS);
+  rpc.send(function(json) {
+    if (json && !json['error']) {
+      that._view.initDailog(json);
+    }
+  });
+};
+
---
+++ drive-web/src/main/webapp/good/drive/person/rightmenu.js
@@ -0,0 +1,43 @@
+'use strict';
+goog.provide('good.drive.person.rigthmenu');
+
+goog.require('good.drive.nav.menu');
+goog.require('good.drive.person.listperson');
+
+
+good.drive.person.rigthmenu.Menu = function(dom) {
+
+  var menu = new good.drive.nav.menu.View();
+  var type = [['i', '详细信息'],['s', ''],
+              ['i', '删除']];
+
+  var corner = {targetCorner: undefined,
+      menuCorner: undefined, contextMenu: true}; 
+  var rightMenu = menu.genPopupMenu(dom, type, undefined, corner);
+  this._rightMenu = rightMenu;
+  this.action();
+};
+
+/**
+ *
+ */
+good.drive.person.rigthmenu.Menu.prototype.action = function() {
+  var that = this;
+  goog.events.listen(that._rightMenu, 'action', function(e) {
+    var grid = good.drive.view.baseview.View.currentGrid;
+    var selectedElemnet = grid.getSelectedItem();
+    var data = selectedElemnet.data; 
+    var list = new good.drive.person.Listperson();
+    var action = e.target.getCaption();
+    switch (action) {     
+      case '详细信息':        
+        list.editPerson(data.userId);
+        break;
+      case '删除':
+        list.deletePerson(data.userId);
+        break;
+      default:
+        break;
+     }     
+  });
+};
