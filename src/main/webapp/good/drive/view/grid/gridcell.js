0d258f3b2f745ee5b94764589d3c31c25e328039
chenyupeng <chenyupeng@goodow.com>
2013-08-15 17:24:08 +0800

    view image size

--- src/main/webapp/good/drive/view/grid/gridcell.js
+++ src/main/webapp/good/drive/view/grid/gridcell.js
@@ -43,7 +43,7 @@ good.drive.view.grid.GridCell.prototype.getImageData =
       if (data.get('thumbnail') != null) {
         return goog.dom.createDom('img', {
           'class' : 'gv-image-el',
-          'src' : data.get('thumbnail') + '=s218'
+          'src' : data.get('thumbnail')
         });
       } else if (data.get('type') == 'audio/mp3') {
         return goog.dom.createDom('img', {
